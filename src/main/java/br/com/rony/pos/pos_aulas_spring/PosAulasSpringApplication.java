4dab1647af76801f4f40d3f9d36288eda7eec52c
Rony Amorim dos Reis <108841655+RonyAmorim@users.noreply.github.com>
2025-05-28 22:56:50 -0300

    Initial commit

---
+++ src/main/java/br/com/rony/pos/pos_aulas_spring/PosAulasSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.rony.pos.pos_aulas_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosAulasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosAulasSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rony/pos/pos_aulas_spring/PosAulasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rony.pos.pos_aulas_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosAulasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
